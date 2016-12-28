082191ea1e6f4d33c80958759f248387d72081c8
= <=>
2016-12-28 12:32:46 +0000

    Exibindo dados CRUD

--- gerente/suporteTeamManager/views.py
+++ gerente/suporteTeamManager/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import ClienteForm
 
 # Create your views here.
 def clientes_list(request):
-    return render(request,'suporteTeamManager/clientes_list.html',{})
+    clientes = ClienteForm.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request,'suporteTeamManager/clientes_list.html',{'clientes' : clientes})
